b69ced23445e05dd84a244b762f495bbae63a2d4
AleksKislov <sinokislov@gmail.com>
2022-03-07 23:10:33 +0300

    chore: comment out function for writing mp3s

--- server/src/api/services/newhskwords/get-audio.js
+++ server/src/api/services/newhskwords/get-audio.js
@@ -3,12 +3,18 @@ const { writeMP3 } = require("./write-mp3");
 
 async function getAudio(req, res) {
   const allLexicon = await Hskword.find({
-    lvl: "6",
+    lvl: "789",
   })
     .select("-ru")
     .sort({ id: 1 });
 
-  await Promise.all(allLexicon.slice(600).map(writeMP3));
+  const NUM = 6;
+
+  const SUM = 900;
+  const start = SUM * NUM;
+  const end = SUM * (NUM + 1);
+  return res.json(allLexicon.slice(5400));
+  // await Promise.all(allLexicon.slice(5400).map(writeMP3));
   res.send("done");
 }
 
