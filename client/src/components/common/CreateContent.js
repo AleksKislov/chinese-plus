47f1de71081227dcd86a07d43c9ee7b95cc04dc8
AleksKislov <sinokislov@gmail.com>
2022-04-30 23:02:32 +0300

    edit: create content page

--- client/src/components/common/CreateContent.js
+++ client/src/components/common/CreateContent.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { CONTENT } from "../../constants/consts.json";
 
-const CreateContent = ({ contentType }) => {
+const CreateContent = () => {
+  const btns = [
+    { ru: "Текст", en: "text" },
+    { ru: "Видео", en: "video" },
+  ];
+
   return (
-    <div className=''>
+    <div>
       <h2>
         Поделитесь текстом или видео <small class='text-muted'>и станьте героем 💪</small>
       </h2>
-
       <p>Каким именно материалом Вы хотели бы поделиться со всем светом?</p>
-
-      <Link className='btn btn-sm btn-info mr-1' to='/create-video'>
-        Видео
-      </Link>
-
-      <Link className='btn btn-sm btn-info' to='/create-text'>
-        Текст
-      </Link>
+      {btns.map(({ ru, en }) => (
+        <Link className='btn btn-sm btn-info mr-2' to={`/create-${en}`}>
+          {ru}
+        </Link>
+      ))}
     </div>
   );
 };
