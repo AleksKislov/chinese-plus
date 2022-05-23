066e32eabf93f8ece00cf586cb70846ef6a00522
AleksKislov <sinokislov@gmail.com>
2022-05-23 10:27:29 +0300

    edit: rewrite reading statistics

--- server/routes/api/services/updateOrCreate.js
+++ server/routes/api/services/updateOrCreate.js
@@ -1,44 +1,46 @@
-const Day = require("../../../src/sql_models/Day");
+const Day = require("../../../src/models/Day");
 const { Op } = require("sequelize");
 
-const errHandler = err => console.log("SQL Error: ", err);
+const errHandler = (err) => console.log("Reading day error: ", err);
 
 /**
  * @param {object} newData  - { user_id<string>, have_read<int>, daily_goal<int> }
  */
-const updateOrCreate = async newData => {
-  // First try to find the record
-  const { user_id } = newData;
-  const date = new Date();
-  const createdAt = date.toISOString().slice(0, 10);
-  const foundDay = await Day.findOne({ where: { user_id, createdAt } }).catch(errHandler);
-
-  if (!foundDay) {
-    // not found, create a new one
-    const day = await Day.create(newData).catch(errHandler);
-    return { day, created: true };
-  }
-
-  // Found and updated it
-  const day = await Day.update(newData, {
-    where: { user_id, createdAt }
+const updateOrCreate = async (newData) => {
+  const { user_id, have_read, daily_goal } = newData;
+  const createdAt = new Date().toISOString().slice(0, 10);
+  const foundDay = await Day.findOne({
+    user_id,
+    createdAt: { $gte: createdAt },
   }).catch(errHandler);
+
+  if (!foundDay) return { day: await new Day(newData).save(), created: true };
+
+  const day = await Day.findByIdAndUpdate(
+    foundDay._id,
+    {
+      $set: { have_read, daily_goal },
+    },
+    { new: true }
+  );
   return { day, created: false };
 };
 
-const fetchReading = async user_id => {
-  const startDate = new Date();
+const fetchReading = async (user_id) => {
+  let startDate = new Date();
   startDate.setMonth(startDate.getMonth() - 1);
   const currentDate = new Date();
 
   const where = {
     user_id,
     createdAt: {
-      [Op.between]: [startDate, currentDate]
-    }
+      // [Op.between]: [startDate, currentDate],
+      $gte: startDate,
+      $lt: currentDate,
+    },
   };
 
-  return await Day.findAll({ where }).catch(errHandler);
+  return await Day.find(where).catch(errHandler);
 };
 
 module.exports = { updateOrCreate, fetchReading };
---
+++ server/src/models/Day.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const DaySchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  have_read: { type: Number },
+  daily_goal: { type: Number },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Day = mongoose.model("day", DaySchema);
