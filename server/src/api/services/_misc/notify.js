ce3cf1f0128c6e9a60627fb4776a5efc16b5c4a5
AleksKislov <sinokislov@gmail.com>
2022-06-01 09:42:55 +0300

    edit: notification msg + const for test tg chat id

--- server/src/api/services/_misc/notify.js
+++ server/src/api/services/_misc/notify.js
@@ -4,18 +4,19 @@ const qs = require("qs");
 const isDevMode = process.env.NODE_ENV === "development";
 
 const tgUrl = process.env.TELEGRAM_NOTICE_URL;
-const tgChannelId = isDevMode ? process.env.TEST_CHAT_ID : process.env.TGCHANNEL_ID;
+const testChatId = process.env.TEST_CHAT_ID;
+const tgChannelId = isDevMode ? testChatId : process.env.TGCHANNEL_ID;
 const vkChannelId = process.env.VKCHANNEL_ID;
 const vkToken = process.env.VKCHANNEL_TOKEN;
 
 class Notify {
   static admin(txt) {
-    return axios.get(encodeURI(`${tgUrl}&text=<pre>${txt}</pre>&chat_id=${testChatId}`));
+    return axios.get(encodeURI(`${tgUrl}&text=${txt}&chat_id=${testChatId}`));
   }
 
   static telegramPublic(txt) {
     try {
-      axios.get(encodeURI(`${tgUrl}&text=${txt})}&chat_id=${tgChannelId}`));
+      axios.get(encodeURI(`${tgUrl}&text=${txt}&chat_id=${tgChannelId}`));
     } catch (e) {
       console.log(e);
     }
@@ -54,15 +55,15 @@ class Notify {
 function getTxt(content) {
   const obj = {};
   if (content.cnSubs) {
-    obj.type = `📺 <strong>Опубликовано новое видео от пользователя</strong> ${content.userName}`;
+    obj.type = `📺 Опубликовано новое видео от пользователя ${content.userName}`;
     obj.link = `https://www.chineseplus.club/videos/${content._id}`;
     obj.desc = content.desc;
   } else if (content.origintext) {
-    obj.type = `📚 <strong>Опубликован новый текст от пользователя</strong> ${content.name}`;
+    obj.type = `📚 Опубликован новый текст от пользователя ${content.name}`;
     obj.link = `https://www.chineseplus.club/texts/${content._id}`;
     obj.desc = content.description;
   } else {
-    obj.type = `🚀 <strong>Новости от админа</strong>`;
+    obj.type = `🚀 Новости от админа`;
     obj.desc = content.text.replace(/\<br \/>/g, "\n");
     obj.link = `https://www.chineseplus.club/posts/${content._id}`;
   }
