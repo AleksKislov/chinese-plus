10dd04450461810316f8d4b46fe1e47a111a85a8
AleksKislov <sinokislov@gmail.com>
2022-06-16 11:21:32 +0300

    chore: remove commented out

--- client/src/components/texts/Audio.js
+++ client/src/components/texts/Audio.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./audio.css";
 import Shikwasa from "shikwasa";
 
 const Audio = ({ title, src }) => {
   useEffect(() => {
-    const player = new Shikwasa({
+    new Shikwasa({
       container: () => document.querySelector("#audioContainer"),
       audio: {
         title,
@@ -14,16 +14,6 @@ const Audio = ({ title, src }) => {
       },
     });
   }, [title, src]);
-
-  // <div className='row'>
-  //   <div className='col-sm-12 my-2'>
-  //     <div className=''>
-  //       <audio controls className='' style={{ width: "100%" }}>
-  //         <source src='sample.mp3' />
-  //       </audio>
-  //     </div>
-  //   </div>
-  // </div>
 
   return <div className='my-2' id='audioContainer'></div>;
 };
