e79f8e3c23952bc8ce9b60d94f068a3ebb43c79f
AleksKislov <sinokislov@gmail.com>
2022-06-22 17:13:36 +0300

    edit: position for filters button

--- client/src/components/texts/common/UnsetFiltersBtn.js
+++ client/src/components/texts/common/UnsetFiltersBtn.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const UnsetFiltersBtn = ({ onClick }) => {
   return (
-    <div className='col-sm-3'>
+    <div className='col-sm-3 d-flex align-items-end flex-column '>
       <div
-        className='btn btn-outline-primary w-100'
+        className='btn btn-outline-primary w-100 mt-auto mb-2'
         onClick={onClick}
         style={{ minHeight: "36.5px" }}
       >
