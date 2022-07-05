9fcaebc89d95f5fc9650392006a738efac16228f
AleksKislov <sinokislov@gmail.com>
2022-07-05 12:11:13 +0300

    feat: add head to pinyin page

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 // import "../styles/Home.module.css";
 // import "../styles/light-theme.css";
 
