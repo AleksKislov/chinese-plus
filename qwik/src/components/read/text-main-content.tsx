0431112e0ee29f18d09e17d54949538cd646b88c
AleksKislov <sinokislov@gmail.com>
2023-11-14 11:37:52 +0800

    qwik refactoring

---
+++ qwik/src/components/read/text-main-content.tsx
@@ -0,0 +1,151 @@
+import { component$, useContext, useSignal, useStore } from "@builder.io/qwik";
+import { MainContent } from "../common/layout/main-content";
+import { Alerts } from "../common/alerts/alerts";
+import { FontSizeBtnGroup } from "../common/content-cards/font-size-btns";
+import { LongTxtPagination } from "./long-txt-pagination";
+import { AudioPlayer } from "./audio-player";
+import { Paragraph } from "./paragraph";
+import { countZnChars } from "~/misc/helpers/content";
+import { CommentsBlockTitle } from "../common/comments/comments-block-title";
+import {
+  type Addressee,
+  CommentForm,
+  type CommentIdToReply,
+  WHERE,
+} from "../common/comments/comment-form";
+import { CommentsBlock } from "../common/comments/comments-block";
+import { EditWordModal } from "../common/modals/edit-word-modal";
+import { MoreInfoModal } from "../common/modals/more-info-modal";
+import { EditChineseArrModal } from "../common/modals/edit-chinese-arr-modal";
+import { userContext } from "~/root";
+import { FontSizeBtns } from "../common/content-cards/content-page-card";
+import type { TextFromDB, TooltipText } from "~/routes/read/texts/[id]";
+import type { CommentType } from "../common/comments/comment-card";
+import { editWordModalId, moreInfoModalId } from "../common/tooltips/word-tooltip";
+
+type TextMainContentProps = {
+  text: TextFromDB & TooltipText & { curPage: number };
+  comments: CommentType[];
+};
+
+export const TextMainContent = component$(({ text, comments }: TextMainContentProps) => {
+  const { isAdmin } = useContext(userContext);
+  const fontSizeSig = useSignal(FontSizeBtns.md);
+  const addressees = useSignal<Addressee[]>([]);
+  const commentIdToReplyStore = useStore<CommentIdToReply>({
+    commentId: "",
+    name: "",
+    userId: "",
+  });
+
+  const {
+    _id: textId,
+    translation,
+    chinese_arr: chineseArr,
+    tooltipTxt,
+    origintext,
+    pages,
+    curPage,
+    audioSrc: hasAudio,
+  } = text;
+
+  const isLongTxt = Boolean(pages && pages.length);
+  const currentWord = useSignal<DictWord | undefined>(undefined);
+  const showTranslation = useSignal(true);
+
+  const editChineseArrModalId = "editChineseArrModalId";
+
+  return (
+    <MainContent>
+      <Alerts />
+
+      <div class={"flex justify-between w-full"}>
+        <div class={"pt-1"}>
+          <FontSizeBtnGroup fontSizeSig={fontSizeSig} />
+        </div>
+        <div>
+          <label class='label cursor-pointer'>
+            <span class='label-text mr-3 font-bold'>
+              {showTranslation.value ? "Без перевода" : "С переводом"}
+            </span>
+
+            <input
+              type='checkbox'
+              class='toggle toggle-accent'
+              checked={!showTranslation.value}
+              onClick$={() => (showTranslation.value = !showTranslation.value)}
+            />
+          </label>
+        </div>
+      </div>
+
+      {isLongTxt && <LongTxtPagination numOfPages={pages.length} curPage={curPage} />}
+
+      {!hasAudio ? null : <AudioPlayer textId={textId} />}
+
+      {tooltipTxt.map((parag, i) => (
+        <Paragraph
+          key={i}
+          fontSize={fontSizeSig.value}
+          tooltipedParag={parag}
+          translation={isLongTxt ? pages[curPage].translation[i] : translation[i]}
+          strLen={countZnChars(isLongTxt ? pages[curPage].origintext[i] : origintext[i])}
+          ind={i}
+          currentWord={currentWord}
+          showTranslation={showTranslation.value}
+        />
+      ))}
+
+      {isAdmin && (
+        <label
+          for={editChineseArrModalId}
+          class={`btn btn-sm btn-outline btn-warning mt-2`}
+          onClick$={() => {}}
+        >
+          edit chinese
+        </label>
+      )}
+
+      <div class={"mt-2"}>
+        <CommentsBlockTitle />
+        <CommentForm
+          contentId={textId}
+          where={WHERE.text}
+          path={undefined}
+          commentIdToReply={commentIdToReplyStore}
+          addressees={addressees}
+        />
+        <CommentsBlock
+          comments={comments}
+          commentIdToReply={commentIdToReplyStore}
+          addressees={addressees}
+        />
+      </div>
+
+      {!currentWord.value ? null : (
+        <div>
+          <EditWordModal word={currentWord.value} modalId={editWordModalId} />
+          <MoreInfoModal
+            word={{
+              _id: currentWord.value._id,
+              cn: currentWord.value.chinese,
+              py: currentWord.value.pinyin,
+              ru: currentWord.value.russian,
+              lvl: "unknown",
+              id: 0,
+            }}
+            modalId={moreInfoModalId}
+          />
+        </div>
+      )}
+
+      {isAdmin && (
+        <EditChineseArrModal
+          chineseArr={chineseArr}
+          modalId={editChineseArrModalId}
+          textId={textId}
+        />
+      )}
+    </MainContent>
+  );
+});
--- qwik/src/routes/read/texts/[id]/index.tsx
+++ qwik/src/routes/read/texts/[id]/index.tsx
@@ -1,37 +1,18 @@
-import { component$, useContext, useSignal, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
 import { ApiService } from "~/misc/actions/request";
-
 import { FlexRow } from "~/components/common/layout/flex-row";
 import { Sidebar } from "~/components/common/layout/sidebar";
-import { MainContent } from "~/components/common/layout/main-content";
-
 import CONSTANTS from "~/misc/consts/consts";
-import { ContentPageCard, FontSizeBtns } from "~/components/common/content-cards/content-page-card";
-import {
-  type Addressee,
-  type CommentIdToReply,
-  WHERE,
-  CommentForm,
-} from "~/components/common/comments/comment-form";
-import { Alerts } from "~/components/common/alerts/alerts";
+import { ContentPageCard } from "~/components/common/content-cards/content-page-card";
+import { WHERE } from "~/components/common/comments/comment-form";
 import { type CommentType } from "~/components/common/comments/comment-card";
 import { getContentComments } from "~/misc/actions/get-content-comments";
-import { CommentsBlock } from "~/components/common/comments/comments-block";
-import { CommentsBlockTitle } from "~/components/common/comments/comments-block-title";
 import { type TextCardInfo } from "..";
-import { parseTextWords, countZnChars } from "~/misc/helpers/content";
-import { Paragraph } from "~/components/read/paragraph";
+import { parseTextWords } from "~/misc/helpers/content";
 import { ContentPageHead } from "~/components/common/ui/content-page-head";
-import { MoreInfoModal } from "~/components/common/modals/more-info-modal";
-import { EditWordModal } from "~/components/common/modals/edit-word-modal";
-import { editWordModalId, moreInfoModalId } from "~/components/common/tooltips/word-tooltip";
-import { FontSizeBtnGroup } from "~/components/common/content-cards/font-size-btns";
-import { LongTxtPagination } from "~/components/read/long-txt-pagination";
-import { AudioPlayer } from "~/components/read/audio-player";
 import { ReadResultCard } from "~/components/me/read-result-card";
-import { userContext } from "~/root";
-import { EditChineseArrModal } from "~/components/common/modals/edit-chinese-arr-modal";
+import { TextMainContent } from "~/components/read/text-main-content";
 
 export type TextContent = {
   origintext: string[];
@@ -78,16 +59,8 @@ export const useGetText = routeLoader$(
 );
 
 export default component$(() => {
-  const { isAdmin } = useContext(userContext);
   const text = useGetText();
   const comments = getComments();
-  const fontSizeSig = useSignal(FontSizeBtns.md);
-  const addressees = useSignal<Addressee[]>([]);
-  const commentIdToReplyStore = useStore<CommentIdToReply>({
-    commentId: "",
-    name: "",
-    userId: "",
-  });
 
   const {
     _id: textId,
@@ -102,21 +75,8 @@ export default component$(() => {
     categoryInd,
     likes,
     length,
-    translation,
-    chinese_arr: chineseArr,
-    tooltipTxt,
-    origintext,
-    pages,
-    curPage,
     source,
-    audioSrc: hasAudio,
   } = text.value;
-
-  const isLongTxt = Boolean(pages && pages.length);
-  const currentWord = useSignal<DictWord | undefined>(undefined);
-  const showTranslation = useSignal(true);
-
-  const editChineseArrModalId = "editChineseArrModalId";
 
   return (
     <>
@@ -142,97 +102,7 @@ export default component$(() => {
           />
           <ReadResultCard />
         </Sidebar>
-
-        <MainContent>
-          <Alerts />
-
-          <div class={"flex justify-between w-full"}>
-            <div class={"pt-1"}>
-              <FontSizeBtnGroup fontSizeSig={fontSizeSig} />
-            </div>
-            <div class={""}>
-              <label class='label cursor-pointer'>
-                <span class='label-text mr-3 font-bold'>
-                  {showTranslation.value ? "Без перевода" : "С переводом"}
-                </span>
-
-                <input
-                  type='checkbox'
-                  class='toggle toggle-accent'
-                  checked={!showTranslation.value}
-                  onClick$={() => (showTranslation.value = !showTranslation.value)}
-                />
-              </label>
-            </div>
-          </div>
-
-          {isLongTxt && <LongTxtPagination numOfPages={pages.length} curPage={curPage} />}
-
-          {!hasAudio ? null : <AudioPlayer textId={textId} />}
-          {tooltipTxt.map((parag, i) => (
-            <Paragraph
-              key={i}
-              fontSize={fontSizeSig.value}
-              tooltipedParag={parag}
-              translation={isLongTxt ? pages[curPage].translation[i] : translation[i]}
-              strLen={countZnChars(isLongTxt ? pages[curPage].origintext[i] : origintext[i])}
-              ind={i}
-              currentWord={currentWord}
-              showTranslation={showTranslation.value}
-            />
-          ))}
-
-          {isAdmin && (
-            <label
-              for={editChineseArrModalId}
-              class={`btn btn-sm btn-outline btn-warning mt-2`}
-              onClick$={() => {}}
-            >
-              edit chinese
-            </label>
-          )}
-
-          <div class={"mt-2"}>
-            <CommentsBlockTitle />
-            <CommentForm
-              contentId={textId}
-              where={WHERE.text}
-              path={undefined}
-              commentIdToReply={commentIdToReplyStore}
-              addressees={addressees}
-            />
-            <CommentsBlock
-              comments={comments.value}
-              commentIdToReply={commentIdToReplyStore}
-              addressees={addressees}
-            />
-          </div>
-
-          {!currentWord.value ? null : (
-            <div>
-              <EditWordModal word={currentWord.value} modalId={editWordModalId} />
-              <MoreInfoModal
-                word={{
-                  _id: currentWord.value._id,
-                  cn: currentWord.value.chinese,
-                  py: currentWord.value.pinyin,
-                  ru: currentWord.value.russian,
-                  lvl: "unknown",
-                  id: 0,
-                }}
-                modalId={moreInfoModalId}
-              />
-            </div>
-          )}
-
-          {isAdmin && (
-            <EditChineseArrModal
-              chineseArr={chineseArr}
-              modalId={editChineseArrModalId}
-              textId={textId}
-            />
-          )}
-        </MainContent>
+        <TextMainContent text={text.value} comments={comments.value} />
       </FlexRow>
     </>
   );
