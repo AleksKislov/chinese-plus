758550431cd47520f4c91fe2f037bdd6248b8436
AleksKislov <sinokislov@gmail.com>
2025-02-10 00:13:19 +0800

    feat: add yandex metrika

--- qwik/src/root.tsx
+++ qwik/src/root.tsx
@@ -9,7 +9,7 @@ import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.
 import { RouterHead } from './components/router-head/router-head';
 
 import globalStyles from './global.css?inline';
-// import { QwikPartytown } from './components/partytown/partytown';
+import { QwikPartytown } from './components/partytown/partytown';
 import type { DonateGoal } from './routes/(club)/donate';
 
 export const IsLightThemeCookieName = 'isLightTheme';
@@ -102,21 +102,32 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         {/* <link rel='manifest' href='/manifest.json' /> */}
-        {/* <QwikPartytown forward={['gtag', 'dataLayer.push']} /> */}
+        <QwikPartytown forward={['ym']} />
         <script
           type="text/partytown"
           dangerouslySetInnerHTML={`
-              !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-              posthog.init('phc_Inl1WaFrAWNUDeSpk5UJ3BD9lJhGZ7Uqx7FyDoGqjm7', {
-                  api_host:'https://us.i.posthog.com',
-                  person_profiles: 'identified_only'
-              })`}
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(48737867, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });            
+            `}
         />
-        {/* <script
-          async
-          type="text/partytown"
-          src="https://www.googletagmanager.com/gtag/js?id=G-PN0P7BPQCV"
-        /> */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/48737867"
+              style="position:absolute; left:-9999px;"
+              alt=""
+            />
+          </div>
+        </noscript>
         <RouterHead />
       </head>
       <body lang="en" class="text-neutral-content">
